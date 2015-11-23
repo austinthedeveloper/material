144d36d2dce96c427edbba08dee83a58035ff850
Robert Messerle <rmesserle@google.com>
2015-11-23 15:24:37 -0800

    fix(progressCircular): fixes scaling issues
    Closes #4839

--- src/components/tabs/js/tabDirective.js
+++ src/components/tabs/js/tabDirective.js
@@ -65,6 +65,7 @@ function MdTab () {
         label = angular.element('<md-tab-label></md-tab-label>');
         if (attr.label) label.text(attr.label);
         else label.append(element.contents());
+
         if (body.length == 0) {
           var contents = element.contents().detach();
           body         = angular.element('<md-tab-body></md-tab-body>');
@@ -114,6 +115,5 @@ function MdTab () {
         }
     );
     scope.$on('$destroy', function () { ctrl.removeTab(data); });
-
   }
 }
